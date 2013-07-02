05b8f6a2d33d6bc4b522c91447f1f0ab38189c49
Yorick Peterse <yorickpeterse@olery.com>
2013-07-02 14:36:18 +0200

    Set the environment name for Airbrake.
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- config/airbrake.rb
+++ config/airbrake.rb
@@ -1,3 +1,4 @@
 Airbrake.configure do |config|
-  config.api_key = '6fc8b369088de85fce0aafd13ca0490c'
+  config.api_key          = '6fc8b369088de85fce0aafd13ca0490c'
+  config.environment_name = 'production'
 end
