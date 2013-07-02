fb516dad3b66015a252a21a76391234edd343968
Yorick Peterse <yorickpeterse@olery.com>
2013-07-02 17:12:53 +0200

    Fix for the Puma config.
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,10 @@ directory   File.expand_path('../../', __FILE__)
 rackup      File.expand_path('../../config.ru', __FILE__)
 state_path  File.expand_path('../../tmp/puma.state', __FILE__)
 
-stdout_redirect File.expand_path('../../log/stdout.log', __FILE__)
-stderr_redirect File.expand_path('../../log/stderr.log', __FILE__)
+stdout_redirect(
+  File.expand_path('../../log/stdout.log', __FILE__),
+  File.expand_path('../../log/stderr.log', __FILE__)
+)
 
 bind        'localhost', 80
 daemonize   true
