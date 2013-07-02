8f11ef61900a324c25bc66f34e4ece02ab8f18b7
Yorick Peterse <yorickpeterse@olery.com>
2013-07-02 16:22:20 +0200

    Specify the PID path for Unicorn.
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,7 @@ listen 80
 
 stdout_path File.expand_path('../../log/stdout.log', __FILE__)
 stderr_path File.expand_path('../../log/stderr.log', __FILE__)
+pid File.expand_path('../../tmp/unicorn.pid', __FILE__)
 
 working_directory File.expand_path('../../', __FILE__)
 worker_processes 5
