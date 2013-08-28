533005947e0272b68f41a4ecc65c400251c28e48
Yorick Peterse <yorickpeterse@olery.com>
2013-08-28 15:59:31 +0200

    Use Rollbar in synchronous mode.

--- config/rollbar.rb
+++ config/rollbar.rb
@@ -6,5 +6,5 @@ Rollbar.configure do |config|
   config.access_token = '419e9e11a03b4d94ba9cdff24867f9e9'
   config.enabled      = %w{production staging}.include?(env)
   config.environment  = env
-  config.use_async    = true
+  config.use_async    = false
 end
